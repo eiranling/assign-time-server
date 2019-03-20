fad886afa2a0ef5e57afb016ea98cd040944eadd
Eiran Ling <eiranling@gmail.com>
2019-03-20 18:27:44 +1300

    Initial commit

---
+++ src/main/java/server/Application.kt
@@ -0,0 +1,11 @@
+package server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
