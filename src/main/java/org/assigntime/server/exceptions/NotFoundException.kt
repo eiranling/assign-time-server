adc042f12f3fe891ec9b39d07350e85c02b2911d
Eiran Ling <eiranling@gmail.com>
2019-03-22 17:23:06 +1300

    Removed constructors for an abstract class

--- src/main/java/org/assigntime/server/exceptions/NotFoundException.kt
+++ src/main/java/org/assigntime/server/exceptions/NotFoundException.kt
@@ -4,7 +4,4 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
-abstract class NotFoundException(message: String, innerException : Exception?) : Exception(message, innerException) {
-    constructor(message: String) : this(message, null)
-    constructor() : this("Not found", null)
-}
+abstract class NotFoundException(message: String, innerException: Exception?) : Exception(message, innerException)
